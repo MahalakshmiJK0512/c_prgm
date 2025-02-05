ada77d1888d9ded522467f700e8903f737b012c9
MahalakshmiJK0512 <163166565+MahalakshmiJK0512@users.noreply.github.com>
2025-02-05 08:40:45 +0530

    basics

---
+++ basics.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float height = 5.9;
+    char grade = 'A';
+    double pi = 3.14159;
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f feet\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Value of Pi: %.5lf\n", pi);
+
+    return 0;
+}
